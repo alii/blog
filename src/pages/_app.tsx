ca0d1283818e6ebc432c2299692bfa78d1d87755
Alistair Smith <alistairsmith01@gmail.com>
2022-01-03 15:17:47 +0000

    fix: _app.tsx page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,15 @@ import {AppProps} from 'next/app';
 
 import 'tailwindcss/tailwind.css';
 import '../styles/main.css';
+import Head from 'next/head';
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
 		<div className="px-4 md:px-16 py-24 max-w-4xl">
+			<Head>
+				<title>alistair smith • blog</title>
+			</Head>
+
 			<Component {...pageProps} />
 		</div>
 	);
