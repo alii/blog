847d85d4bd3eb8ecea8d3f955a3e6a13aa991071
Alistair Smith <alistairsmith01@gmail.com>
2022-01-06 23:54:40 +0000

    feat: render mochip in prod, hidden

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,10 +1,10 @@
+import {Post} from './Post';
+import {ensure} from '../types';
+
 import {Goals} from './2022/01/goals/goals';
 import {ServerlessDiscordOAuth} from './2022/01/serverless-discord-oauth/serverless-discord-oauth';
 import {Mochip} from './2022/01/mochip/mochip';
-import {Post} from './Post';
 
-export const posts: Post[] = [
-	process.env.NODE_ENV === 'development' && new Mochip(),
-	new ServerlessDiscordOAuth(),
-	new Goals(),
-].filter(post => typeof post !== 'boolean') as Post[];
+const ensurePosts = ensure<Post[]>();
+
+export const posts = ensurePosts([new Mochip(), new ServerlessDiscordOAuth(), new Goals()]);
