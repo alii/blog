334f48183b66858ff4428df8098ad1853ff090b2
Alistair Smith <hi@alistair.sh>
2022-09-23 00:14:03 +0100

    feat: add hits.link url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,32 @@ import '../styles/main.css';
 import React from 'react';
 import Head from 'next/head';
 import type {AppProps} from 'next/app';
+import urlcat from 'urlcat';
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({Component, pageProps, router}: AppProps) {
 	return (
-		<div className="px-4 md:px-16 py-24 max-w-4xl">
+		<div className="px-4 md:px-16 space-y-24 py-24 max-w-4xl">
 			<Head>
 				<title>alistair smith • blog</title>
 			</Head>
 
 			<Component {...pageProps} />
+
+			<footer className="space-y-2">
+				<a
+					href="https://alistair.sh"
+					className="underline decoration-blue-500/20 hover:decoration-blue-500/50 text-neutral-400 dark:text-neutral-700"
+				>
+					Alistair Smith
+				</a>
+
+				<img
+					className="opacity-50"
+					src={urlcat('https://hits.link', '/hits', {
+						url: urlcat('https://alistair.blog', router.asPath),
+					})}
+				/>
+			</footer>
 		</div>
 	);
 }
