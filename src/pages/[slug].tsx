c0da1dee149eba0636691497ae47234a20446c17
Alistair Smith <hi@alistair.sh>
2025-05-09 03:10:27 -0700

    use gray-950 for theme color

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -22,7 +22,7 @@ export default function PostPage({slug}: Props) {
 				<title>{post.name}</title>
 				<meta name="description" content={post.excerpt} />
 				<meta name="keywords" content={post.keywords.join(', ')} />
-				<meta name="theme-color" content={post.hidden ? '#ebb305' : '#171717'} />
+				<meta name="theme-color" content={post.hidden ? '#ebb305' : '#020711'} />
 				<meta property="og:image" content={`https://alistair.blog/api/og?slug=${post.slug}`} />
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:title" content={post.name} />
